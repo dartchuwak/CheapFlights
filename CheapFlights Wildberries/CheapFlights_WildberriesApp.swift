c6beb5be5632403afc67bc655020d9c10b10b011
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-08-28 12:06:14 +0300

    Initial Commit

---
+++ CheapFlights Wildberries/CheapFlights_WildberriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheapFlights_WildberriesApp.swift
+//  CheapFlights Wildberries
+//
+//  Created by Evgenii Mikhailov on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CheapFlights_WildberriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
